f92ecf90e8e364115997903eee95fe38e2177e2f
Emmanouil Papadeas <manoschool@yahoo.gr>
2022-12-03 23:28:30 +0200

    Day 3 part two

--- src/day3/main.rs
+++ src/day3/main.rs
@@ -18,13 +18,14 @@ fn main() {
     let content: String = fs::read_to_string(file_path)
         .expect("File failed to read");
     let mut priority_sum_1: usize = 0;
+    let mut priority_sum_2: usize = 0;
     let rucksacks: Vec<&str> = content.split('\n').collect();
+    // Part 1
     for ruck in rucksacks.iter() {
         let half_len: usize = ruck.len() / 2;
         if half_len < 1 {
             continue;
-        }
-        // Part 1
+        }     
         let first: &str = &ruck[..half_len];
         let second: &str = &ruck[half_len..];
         let mut shared: Vec<char> = Vec::new();
@@ -33,10 +34,27 @@ fn main() {
                 shared.push(item);
                 let priority: usize = find_priority(item) as usize;
                 priority_sum_1 += priority;
-                //println!("{item} {priority}");
             }
         }
     }
-
+    // Part 2
+    let step: usize = 3;
+    for i in ((step - 1)..rucksacks.len()).step_by(step) { // Iterate through the vector with a step of 3, starting at index 2
+        let first: &str = rucksacks[i - 2];
+        let second: &str = rucksacks[i - 1];
+        let third: &str = rucksacks[i];
+        if third.len() < 1 {
+            continue
+        }
+        let mut shared: char = '0'; // Temporary placeholder char
+        for item in third.chars() {
+            if first.contains(item) && second.contains(item) && shared != item {
+                shared = item;
+            }
+        }
+        let priority: usize = find_priority(shared) as usize;
+        priority_sum_2 += priority;
+    }
     println!("The sum of the priorities of each rucksack is: {priority_sum_1}");
+    println!("The sum of the priorities of each group is: {priority_sum_2}");
 }
